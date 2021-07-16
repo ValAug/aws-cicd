de6bd8b9d65a1bb00fa3af537bbe7c2f7703d5a4
AuVal <valdiviaaugusto9@gmail.com>
2021-07-16 13:38:14 -0400

    pipeline deployment completed it

--- s3.tf
+++ s3.tf
@@ -3,3 +3,9 @@ resource "aws_s3_bucket" "codepipeline" {
   acl = "private"
   force_destroy = true
 }
+
+resource "aws_s3_bucket" "codepipeline2" {
+  bucket = "pipeline-uhn2"
+  acl = "private"
+  force_destroy = true
+}
